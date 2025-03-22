691aa2be29743a4379d2a5c2ad7949bc0b84f333
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2025-03-23 04:20:30 +0800

    add form components including FormControl, FormDescription, FormItem, FormLabel, FormMessage, and RadioGroup with utility functions

---
+++ components/ui/form/FormLabel.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import type { LabelProps } from 'reka-ui';
+import type { HTMLAttributes } from 'vue';
+import { cn } from '@utils/lib/utils';
+import { Label } from '@input/components/ui/label';
+import { useFormField } from './useFormField';
+
+const props = defineProps<LabelProps & { class?: HTMLAttributes['class'] }>();
+
+const { error, formItemId } = useFormField();
+</script>
+
+<template>
+  <Label :class="cn(error && 'text-destructive', props.class)" :for="formItemId">
+    <slot />
+  </Label>
+</template>
