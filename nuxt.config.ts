ccd15f238fd1389c2d6e9879411408802ad3732b
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2025-05-22 15:23:49 +0800

    Refactor nuxt.config.ts and package.json: update module imports and add new dependencies for improved functionality

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,25 @@
+import tailwindcss from '@tailwindcss/vite';
+import VueDevTools from 'vite-plugin-vue-devtools';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@nuxtjs/tailwindcss',
     'shadcn-nuxt',
     '@nuxtjs/color-mode',
     '@nuxt/eslint',
     '@formkit/auto-animate/nuxt',
     '@pinia/nuxt',
     'pinia-plugin-persistedstate/nuxt',
+    'shadcn-nuxt',
   ],
   css: ['@/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+      VueDevTools({
+        launchEditor: 'webstorm',
+      }),
+    ],
+  },
   shadcn: {
     /**
      * Prefix for all the imported component
@@ -25,14 +35,19 @@ export default defineNuxtConfig({
     classSuffix: '',
   },
   eslint: {
-    // Optional: Customize module options
-    lintOnStart: false, // Disable linting on startup for faster dev
+    lintOnStart: false,
   },
   runtimeConfig: {
     public: {
-      apiBase: process.env.API_BASE_URL || 'http://localhost:4000', // Backend URL
+      apiBase: process.env.API_BASE_URL || 'http://localhost:4000/api', // Backend URL
     },
   },
+  plugins: ['@/plugins/vue-query.ts'],
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+    vscode: {
+      enabled: false,
+    },
+  },
 });
