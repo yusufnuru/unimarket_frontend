b17eefc1126c588f1ef88f14881ad1188e61cf6e
Yusuf-Nuru-Yesuf <yusufnuru2002@gmail.com>
2025-05-24 15:15:53 +0800

    Refactor email verification page: update loading state handling, improve error messaging, and enhance UI feedback

--- pages/email/verify/[code].vue
+++ pages/email/verify/[code].vue
@@ -1,23 +1,88 @@
+<script setup lang="ts">
+import { toast } from 'vue-sonner';
+import { AlertCircle, CheckCircle2 } from 'lucide-vue-next';
+import { useRoute } from 'vue-router';
+import { Alert, AlertDescription } from '@/components/ui/alert';
+import { useAuthStore } from '@/stores/authStore';
+import { AxiosError } from 'axios';
+import { useQuery } from '@tanstack/vue-query';
+import type { AuthResponseMessage } from '@/types/auth';
+definePageMeta({
+  layout: 'auth',
+  middleware: ['auth-redirect'],
+});
+
+const route = useRoute();
+const router = useRouter();
+const code = route.params.code as string;
+const { isLoading, error, isError, isSuccess } = useQuery<AuthResponseMessage>({
+  queryKey: ['verifyEmail', code],
+  queryFn: async () => {
+    const auth = useAuthStore();
+    const response = await auth.verifyEmail(code);
+
+    toast.success('Email verified successfully', {
+      description: response.message,
+      duration: 3000,
+    });
+    setTimeout(async () => {
+      await router.push('/');
+    }, 2000);
+    return response;
+  },
+  enabled: !!code,
+  retry: false,
+});
+
+const errorMessage = computed(() => {
+  if (error.value instanceof AxiosError) {
+    return error.value.response?.data?.message as string;
+  }
+  return 'Verification failed, An unexpected error occurred.';
+});
+</script>
+
 <template>
   <div class="flex min-h-screen justify-center mt-12">
     <div class="container mx-auto max-w-md py-12 px-6 text-center">
-      <div v-if="loading" class="flex justify-center">
-        <Spinner />
+      <div v-if="isLoading" class="flex justify-center">
+        <div class="text-center">
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              class="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-black-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span class="sr-only">Verifying Email...</span>
+          </div>
+        </div>
       </div>
       <div v-else class="space-y-6">
-        <Alert :variant="success ? 'default' : 'destructive'" class="w-fit mx-auto rounded-xl">
-          <AlertCircle v-if="!success" class="h-4 w-4" />
-          <CheckCircle2 v-else class="h-4 w-4" />
+        <Alert
+          :variant="isSuccess ? 'default' : 'destructive'"
+          class="w-fit mx-auto rounded-xl flex items-center"
+        >
+          <CheckCircle2 v-if="isSuccess" class="h-4 w-4 text-green-600" />
+          <AlertCircle v-else class="h-4 w-4 text-red-600" />
           <AlertDescription class="ml-2">
-            {{ success ? 'Email Verified!' : errorMessage }}
+            {{ isSuccess ? 'Email Verified!' : errorMessage }}
           </AlertDescription>
         </Alert>
 
-        <p v-if="!success" class="text-muted-foreground">
+        <p v-if="isError" class="text-muted-foreground">
           The link is either invalid or expired.
-          <NuxtLink to="/password/forgot" class="text-primary hover:underline">
-            Get a new link
-          </NuxtLink>
+          <NuxtLink to="/login" class="text-primary hover:underline"> Get a new link </NuxtLink>
         </p>
 
         <NuxtLink to="/" class="text-primary hover:underline"> Back to home </NuxtLink>
@@ -25,64 +90,3 @@
     </div>
   </div>
 </template>
-
-<script setup lang="ts">
-import { toast } from '@/components/ui/toast';
-import { ref, onMounted } from 'vue';
-import { useRoute } from 'vue-router';
-import { Alert, AlertDescription } from '@/components/ui/alert';
-import { useAuthStore } from '@/stores/useAuthStore';
-import { AxiosError } from 'axios';
-
-definePageMeta({
-  layout: 'auth',
-  middleware: ['auth-redirect'],
-});
-
-const auth = useAuthStore();
-const route = useRoute();
-const code = route.params.code as string; // Dynamic route param
-const loading = ref(true);
-const success = ref(false);
-const errorMessage = ref('Invalid Link');
-
-const verify = async () => {
-  try {
-    await auth.verifyEmail(code); // Call the store method
-    success.value = true;
-    setTimeout(() => {
-      void navigateTo('/');
-    }, 3000);
-
-    toast({
-      title: 'Email Successfully Verified',
-      description: 'You can now login to your account',
-      variant: 'default',
-      duration: 3000,
-    });
-  } catch (error: unknown) {
-    success.value = false;
-
-    let errorMessage = 'Account Verfication Failed';
-    if (error instanceof AxiosError) {
-      errorMessage = error.response?.data?.message;
-    } else if (typeof error === 'string') {
-      errorMessage = error;
-    }
-
-    toast({
-      title: 'Verification Failed',
-      description: errorMessage,
-      variant: 'destructive',
-      duration: 3000,
-    });
-    // Customize error message based on backend response if possible
-  } finally {
-    loading.value = false;
-  }
-};
-
-onMounted(async () => {
-  await verify();
-});
-</script>
