a082e907164d6f47299a5e1af697bdbc61278cff
yusufnuru <yusufnuru2002@gmail.com>
2025-06-21 15:55:43 +0800

    fix: refactor Pagination component structure for improved readability and maintainability

--- components/Pagination.vue
+++ components/Pagination.vue
@@ -25,38 +25,44 @@ defineProps<{
 </script>
 
 <template>
-  <Pagination class="mt-4" :items-per-page="itemsPerPage" :total="totalItems">
-    <PaginationContent>
-      <PaginationPrevious @click="goToPreviousPage" />
+  <div>
+    <Pagination class="mt-4" :items-per-page="itemsPerPage" :total="totalItems">
+      <PaginationContent>
+        <PaginationPrevious @click="goToPreviousPage" />
 
-      <PaginationItem v-if="currentPage > 3" :value="1" @click="goToFirstPage">
-        <PaginationFirst />
-      </PaginationItem>
-
-      <PaginationEllipsis v-if="currentPage > 4" />
-
-      <template v-for="pageNum in getPageNumbers()" :key="pageNum">
-        <PaginationItem
-          :value="Number(pageNum)"
-          :is-active="pageNum === currentPage"
-          @click="goToPage(Number(pageNum))"
-        >
-          {{ pageNum }}
+        <PaginationItem v-if="currentPage > 3" :value="1" @click="goToFirstPage">
+          <PaginationFirst />
         </PaginationItem>
-      </template>
 
-      <PaginationEllipsis v-if="currentPage < totalPages - 3" />
+        <PaginationEllipsis v-if="currentPage > 4" />
 
-      <PaginationItem v-if="currentPage < totalPages - 2" :value="totalPages" @click="goToLastPage">
-        <PaginationLast />
-      </PaginationItem>
+        <template v-for="pageNum in getPageNumbers()" :key="pageNum">
+          <PaginationItem
+            :value="Number(pageNum)"
+            :is-active="pageNum === currentPage"
+            @click="goToPage(Number(pageNum))"
+          >
+            {{ pageNum }}
+          </PaginationItem>
+        </template>
 
-      <PaginationNext @click="goToNextPage" />
-    </PaginationContent>
-  </Pagination>
+        <PaginationEllipsis v-if="currentPage < totalPages - 3" />
 
-  <div v-if="totalItems > 0" class="text-center text-sm text-muted-foreground mt-4">
-    Showing {{ (currentPage - 1) * itemsPerPage + 1 }} -
-    {{ Math.min(currentPage * itemsPerPage, totalItems) }} of {{ totalItems }} items
+        <PaginationItem
+          v-if="currentPage < totalPages - 2"
+          :value="totalPages"
+          @click="goToLastPage"
+        >
+          <PaginationLast />
+        </PaginationItem>
+
+        <PaginationNext @click="goToNextPage" />
+      </PaginationContent>
+    </Pagination>
+
+    <div v-if="totalItems > 0" class="text-center text-sm text-muted-foreground mt-4">
+      Showing {{ (currentPage - 1) * itemsPerPage + 1 }} -
+      {{ Math.min(currentPage * itemsPerPage, totalItems) }} of {{ totalItems }} items
+    </div>
   </div>
 </template>
